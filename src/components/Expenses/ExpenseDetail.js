374d67be127ea2d7bbd6765568649b32d698486e
bhavesh029 <chouhanbhavesh9@gmail.com>
2022-05-28 13:37:41 +0530

    adding buttons

--- src/components/Expenses/ExpenseDetail.js
+++ src/components/Expenses/ExpenseDetail.js
@@ -1,11 +1,21 @@
-import react from 'react';
+import react, {useState} from 'react';
 import './ExpenseItems.css';
 
 function ExpenseDetail(props){
+    const [title, setTitle] = useState(props.title);
+    const [amount, setAmount] = useState(props.amount);
+    let clickHandler = () => {
+        setTitle('Updated');
+    }
+    let priceHandler = () => {
+        setAmount(250);
+    }
     return <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <h3>{props.type}</h3>
-        <div className='expense-item__price'>$ {props.amount}</div>
+        <div className='expense-item__price'>$ {amount}</div>
+        <button onClick={clickHandler}>Update</button>
+        <button onClick={priceHandler}>Change Amount</button>
     </div>
 }
 
